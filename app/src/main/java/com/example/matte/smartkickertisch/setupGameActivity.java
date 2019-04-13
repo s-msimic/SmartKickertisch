ee328774acb0fe426e4d0546d77fef1004616c41
StefanSandner <s-ssand1@haw-landshut.de>
2019-04-13 20:59:59 +0200

    Added Return button to setupGameActivity

--- app/src/main/java/com/example/matte/smartkickertisch/setupGameActivity.java
+++ app/src/main/java/com/example/matte/smartkickertisch/setupGameActivity.java
@@ -1,5 +1,6 @@
 package com.example.matte.smartkickertisch;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -35,6 +36,13 @@ public class setupGameActivity extends AppCompatActivity {
     PlayerButtonTag bottomRightButton;
     List<PlayerButtonTag> players = new ArrayList<>();
     Button deleteButton;
+
+    public void onClickReturn(View view){
+
+        ref.child("lobby").child(lobbyPath).removeValue();
+        Intent i = new Intent(setupGameActivity.this, MenuFolderActivity.class);
+        startActivity(i);
+    }
 
     public void onClickDelete(View view){
         if (!isWiggling) {
