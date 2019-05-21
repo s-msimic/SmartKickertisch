0b59d615122b2df82ee213345e116a6b74aab50c
StefanSandner <s-ssand1@haw-landshut.de>
2019-05-21 17:02:01 +0200

    New branch for lobby changes

--- app/src/main/java/com/example/matte/smartkickertisch/LobbyActivity.java
+++ app/src/main/java/com/example/matte/smartkickertisch/LobbyActivity.java
@@ -1,9 +1,6 @@
 package com.example.matte.smartkickertisch;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.service.autofill.FieldClassification;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +11,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
